b68c3453699f71235256ed9f276edd1d09b760ed
Yaw Ntiri <yawntiri33@gmail.com>
2024-01-06 17:29:04 +0000

    updates replaced some files

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const appRoutes = require('./routes/route.js');
-// data should print in json format
-app.use(express.json());
 
-//middleware to handle api endpoint requests
-app.use('/api', appRoutes)
+
+// --- Middlewares ---
+
+app.use(express.json());// express should accept data in json format
+app.use(express.urlencoded());// express decodes data sent through html
+app.use(express.static('public'));//rendering application on local host
+app.use('/', appRoutes)// routes end point
+
+
 app.listen(port, ()=>{
   console.log(`server is live at ${port}`);
 })
