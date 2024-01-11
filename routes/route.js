8eb6ee4922a792e0fa6cb9966bcfbb9c2b71d6c3
Yaw Ntiri <yawntiri33@gmail.com>
2024-01-11 17:54:00 +0000

    adding email feature still in progress

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const {mymailer, getapp, getform} = require('../controller/appController.js');
+const {mymailer, getapp} = require('../controller/appController.js');
 
 router.post('/myMailer', mymailer);
-router.get('/getApp', getapp);
-router.post('/getForm', getform);
+router.post('/getApp', getapp);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const appRoutes = require('./routes/route.js');
 // --- Middlewares ---
 
 app.use(express.json());// express should accept data in json format
-app.use(express.urlencoded({extended: true}));// express decodes data sent through html
+app.use(express.urlencoded());// express decodes data sent through html
 // app.use(express.static('public'));//rendering application on local host
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));// renders application
 app.use('/', appRoutes)// routes end point
 
 
